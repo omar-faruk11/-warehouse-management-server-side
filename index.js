e94f59330c01bd9f2fe2a52348b071f7f72997f8
omar-faruk11 <omarabdullah1811@gmail.com>
2022-04-30 00:38:10 +0600

    Server ready to work

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port =process.env.PROT || 5000;
+
+
+
+app.use(express.json());
+app.use(cors())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tiivm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log('db connected');
+  // perform actions on the collection object
+  client.close();
+});
+
+
+app.get('/',(req,res) =>{
+    res.send('Hi, I am your awesome server')
+})
+
+app.listen(port,()=>{
+    console.log(`server running ${port}`);
+})
