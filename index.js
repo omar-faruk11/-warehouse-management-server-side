be256fa4c81eb6fbe26c5b355091ae5db0a3e580
omar-faruk11 <omarabdullah1811@gmail.com>
2022-04-30 11:41:50 +0600

    Read all data

--- index.js
+++ index.js
@@ -13,15 +13,26 @@ app.use(cors())
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tiivm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tiivm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('db connected');
-  // perform actions on the collection object
-  client.close();
-});
-
+async function run () {
+  try{
+    await client.connect();
+    const collection = client.db("bikeItems").collection("bikes");
+    console.log('connected');
+    // read all data
+    app.get('/bikes',async(req,res)=>{
+      const q = req.query;
+      const coursor = collection.find(q);
+      const result =await coursor.toArray();
+      res.send(result);
+    })
+  }
+  finally{
+    // await client.close();
+  }
+}
+run().catch(console.dir)
 
 app.get('/',(req,res) =>{
     res.send('Hi, I am your awesome server')
