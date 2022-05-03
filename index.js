1c12f8483fbe7c2be9590bb3270ac8d485918094
omar-faruk11 <omarabdullah1811@gmail.com>
2022-05-03 17:28:25 +0600

    Post data added

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const port =process.env.PORT || 5000;
+const {
+  MongoClient,
+  ServerApiVersion,
+  ObjectId
+} = require('mongodb');
+const port = process.env.PORT || 5000;
 
 
 
@@ -13,41 +17,63 @@ app.use(cors())
 
 
 
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tiivm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-async function run () {
-  try{
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tiivm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1
+});
+async function run() {
+  try {
     await client.connect();
     const productCollection = client.db("bikeItems").collection("bikes");
     console.log('connected');
     // read all data
-    app.get('/products',async(req,res)=>{
+    app.get('/products', async (req, res) => {
       const q = req.query;
       const coursor = productCollection.find(q);
-      const result =await coursor.toArray();
+      const result = await coursor.toArray();
       res.send(result);
     });
-
+    // get products by Email addrress 
+    app.get('/products', async (req, res) => {
+      const q = req.body;
+      const coursor = productCollection.find(q);
+      const result = await coursor.toArray();
+      res.send(result);
+    });
     // read a data by id
     // http://localhost:5000/bikes/626c843b9e3c0e5df2c06ce9
-    app.get('/products/:id',async(req,res)=>{
+    app.get('/products/:id', async (req, res) => {
       const id = req.params.id;
-      const find = {_id:ObjectId(id)};
+      const find = {
+        _id: ObjectId(id)
+      };
       const result = await productCollection.findOne(find)
       res.send(result)
     });
-    
+    // post data 
+    app.post('/product', async (req, res) => {
+      const data = req.body;
+      const result = await productCollection.insertOne(data)
+      res.send(result);
+    });
+
     //  Update data using id 
     // http://localhost:5000/bike/626c843b9e3c0e5df2c06ce9
-    app.put('/product/:id',async(req,res)=>{
+    app.put('/product/:id', async (req, res) => {
       const id = req.params.id;
-      const currentWuantity = req.body.newQuantity;
-      const filter = {_id: ObjectId(id)};
-      const options = { upsert: true };
+      const currentQuantity = req.body.newQuantity;
+      const filter = {
+        _id: ObjectId(id)
+      };
+      const options = {
+        upsert: true
+      };
       const updateDoc = {
 
         $set: {
-          quantity:currentWuantity
+          quantity: currentQuantity
         },
       };
       const result = await productCollection.updateOne(filter, updateDoc, options);
@@ -55,24 +81,25 @@ async function run () {
     });
 
     // Delete a data using id
-    app.delete('/product/:id',async(req,res)=>{
+    app.delete('/product/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: ObjectId(id)}
-      const result =await productCollection.deleteOne(filter)
+      const filter = {
+        _id: ObjectId(id)
+      }
+      const result = await productCollection.deleteOne(filter)
       res.send(result)
     })
-    
-  }
-  finally{
+
+  } finally {
     // await client.close();
   }
 }
 run().catch(console.dir)
 
-app.get('/',(req,res) =>{
-    res.send('Hi, I am your awesome server')
+app.get('/', (req, res) => {
+  res.send('Hi, I am your awesome server')
 });
 
-app.listen(port,()=>{
-    console.log(`server running ${port}`);
+app.listen(port, () => {
+  console.log(`server running ${port}`);
 })
